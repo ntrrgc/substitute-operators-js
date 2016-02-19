f99fb0d8a43714a644770b992855762c796666f3
Juan Luis Boya García <ntrrgc@gmail.com>
2016-02-20 00:00:22 +0100

    Better operator implementation

--- operators-implementation.js
+++ operators-implementation.js
@@ -2,62 +2,40 @@
  * Created by ntrrgc on 17/01/16.
  */
 
-var operatorsCalledWithArrays = [];
+var operatorsCalledWithArrays = {};
 
-function LT(opId, a, b) {
-  if (!Array.isArray(a)) {
-    return a < b;
-  } else {
-    // 2d compare
-    operatorsCalledWithArrays.push(opId);
-    if (a[0] !== b[0]) {
-      return a[0] < b[0];
+function _lt(a,b) {return a < b}
+function _lte(a,b) {return a <= b}
+function _gt(a,b) {return a > b}
+function _gte(a,b) {return a >= b}
+
+
+function operatorBase(op) {
+  return function (opId, a, b) {
+    var jsResult = op(a,b)
+    var altResult;
+
+    var is2dArray = (Array.isArray(a) && a.length == 2);
+    if (!is2dArray) {
+       altResult = op(a, b);
     } else {
-      return a[1] < b[1];
+      // 2d compare
+      operatorsCalledWithArrays[opId] = true;
+      if (a[0] !== b[0]) {
+        altResult = op(a[0], b[0]);
+      } else {
+        altResult = op(a[1], b[1]);
+      }
     }
+
+    if (jsResult !== altResult) {
+      //console.log('%d caused different behavior (%s, %s)', opId, jsResult, altResult);
+    }
+    return altResult;
   }
 }
 
-function GT(opId, a, b) {
-  if (!Array.isArray(a)) {
-    return a < b;
-  } else {
-    // 2d compare
-    operatorsCalledWithArrays.push(opId);
-    if (a[0] !== b[0]) {
-      return a[0] > b[0];
-    } else {
-      return a[1] > b[1];
-    }
-  }
-}
-
-function LTE(opId, a, b) {
-  if (!Array.isArray(a)) {
-    return a <= b;
-  } else {
-    // 2d compare
-    operatorsCalledWithArrays.push(opId);
-    if (a[0] !== b[0]) {
-      return a[0] <= b[0];
-    } else {
-      return a[1] <= b[1];
-    }
-  }
-}
-
-function GTE(opId, a, b) {
-  if (!Array.isArray(a)) {
-    return a >= b;
-  } else {
-    // 2d compare
-    operatorsCalledWithArrays.push(opId);
-    if (a[0] !== b[0]) {
-      return a[0] >= b[0];
-    } else {
-      return a[1] >= b[1];
-    }
-  }
-}
-
-
+var LT = operatorBase(_lt)
+var GT = operatorBase(_gt)
+var GTE = operatorBase(_gte)
+var LTE = operatorBase(_lte)
