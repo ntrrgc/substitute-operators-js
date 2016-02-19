7d9e28b7fb27379825c7db4da0f5802a1942106d
Juan Luis Boya García <ntrrgc@gmail.com>
2016-02-19 01:59:39 +0100

    support several files with glob

--- main.js
+++ main.js
@@ -2,40 +2,69 @@ var util = require('util');
 var recast = require("recast");
 var fs = require('fs');
 var process = require('process');
+var argparse = require('argparse')
+var glob = require('glob')
 var b = recast.types.builders;
- 
-// Let's turn this function declaration into a variable declaration. 
-var code = fs.readFileSync(process.argv[2]);
 
-// Parse the code using an interface similar to require("esprima").parse. 
-var ast = recast.parse(code);
+var parser = new argparse.ArgumentParser({
+  version: '1.0',
+  addHelp: true,
+  description: 'Instruments a code replacing all comparison operators with function calls.'
+});
+
+parser.addArgument(['glob'], {
+  help: 'A glob matching the files that will be instrumented. You better have a backup of them since they will be' +
+  ' edited in place.'
+});
+
+var args = parser.parseArgs()
+
+var files = glob(args.glob, {
+  sync: true,
+  sorted: true,
+  nodir: true,
+});
 
 var operatorCount = 1;
 
-recast.types.visit(ast, {
-    visitBinaryExpression: function(path) {
-        var node = path.value;
+files.forEach(function (file) {
+  console.log('Processing %s', file);
+  // Let's turn this function declaration into a variable declaration.
+  var code = fs.readFileSync(file);
 
-        var operators = {
-            '<': 'LT',
-            '<=': 'LTE',
-            '>': 'GT',
-            '>=': 'GTE'
-        };
+  // Parse the code using an interface similar to require("esprima").parse.
+  var ast = recast.parse(code);
 
-        if (node.operator in operators) {
-            var functionName = operators[node.operator];
+  recast.types.visit(ast, {
+    visitBinaryExpression: function (path) {
+      var node = path.value;
 
-            var replacement = b.callExpression(b.identifier(functionName), [
-              b.literal(operatorCount++),
-              node.left,
-              node.right
-            ]);
-            path.replace(replacement)
-        }
+      var operators = {
+        '<': 'LT',
+        '<=': 'LTE',
+        '>': 'GT',
+        '>=': 'GTE'
+      };
 
-        this.traverse(path);
+      if (node.operator in operators) {
+        var functionName = operators[node.operator];
+
+        var replacement = b.callExpression(b.identifier(functionName), [
+          b.literal(operatorCount++),
+          node.left,
+          node.right
+        ]);
+        path.replace(replacement)
+      }
+
+      this.traverse(path);
     }
-})
+  });
 
-console.log(recast.print(ast).code);
+  var newCode = recast.print(ast).code;
+
+  // Replace file in-place
+  fs.writeFileSync(file, newCode);
+});
+
+console.log('Done');
